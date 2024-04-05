bc35738c585a6ca347050e09d50c16fed0910421
Vinayak6361 <166117611+Vinayak6361@users.noreply.github.com>
2024-04-05 14:57:27 +0530

    Add files via upload

---
+++ withoutfunc.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int b,t,d,g;
+    cin>>b;
+    
+    t=(10.0/100)*b;
+    d=(12.0/100)*b;
+    g=b+t+d;
+    cout<<g<<endl;
+    
+    
+
+    return 0;
+}
